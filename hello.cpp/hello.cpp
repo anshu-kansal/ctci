c2cb1969492cd184d600f4d98f5cffa4efd30251
Anshu Kansal <anshukansal77970.sre@gmail.com>
2023-12-29 15:05:42 +0530

    day 1

--- hello.cpp/hello.cpp
+++ hello.cpp/hello.cpp
@@ -1,6 +1,41 @@
-#include<cstdio>
-int main()
-{
-    printf("Hello,World!");
-    return 0;
-}
+#include<iostream>
+using namespace std;
+// int main()
+// {
+//    cout<<("Hello, World!");
+//    return 0;
+// }
+
+// int main()
+// {
+//    int the_answer=42;
+//     int lucky_number= the_answer/6;
+//     cout<< lucky_number;
+//     return 0;
+// }
+
+// int main()
+// {
+//    int a = 3;
+//     int b = 5;
+//    float c = 43.2;
+//    bool d = true  ;
+//     cout<<c/a;
+//     return 0;
+// }
+// int main()
+// {
+//     int x = 100;
+//     if(x > 100){
+//         cout<< "X is greater than 100 "<<endl;
+//         cout<< x ;
+//     }  
+//     else if(x==100){
+//         cout<<"X is equal to 100";
+//     }
+//     else{
+//         cout<<"X is less than 100";
+//     }
+
+
+    
